7add548047836e7ee711babb8cd138751ac69654
jimmy.chen <jimmy.chen@savitech.co>
2025-09-06 19:13:44 +0800

    Upgrade to LHDC AOSP V5.2.5
    1. V5.2.5
    2. Fix LHDC 5.0 lossless 96 KHZ issue
    3. Change LHDC 5.0 bitrate stage:128kbps to 160kbps
    4. Obsolete LHDC V2.0
    
    MAIN_PROJ_LHDC - 6d8566
    LHDC_AOSP13_V5.2.5

--- inc/lhdcv5BT.h
+++ inc/lhdcv5BT.h
@@ -18,6 +18,12 @@ int32_t lhdcv5BT_get_handle
     HANDLE_LHDCV5_BT	*handle
 );
 
+int32_t lhdcv5BT_set_lossless_raw_enable
+(
+    HANDLE_LHDCV5_BT  handle,
+    uint32_t          enabled
+);
+
 int32_t lhdcv5BT_get_bitrate
 (
     HANDLE_LHDCV5_BT	handle,
--- inc/lhdcv5BT_dec.h
+++ inc/lhdcv5BT_dec.h
@@ -30,11 +30,12 @@ extern "C" {
 
 typedef struct  
 {
-  lhdc_ver_t version;
+  lhdcv5_ver_t version;
   uint32_t sample_rate;
   uint32_t bits_depth;
   uint32_t bit_rate;
   uint32_t lossless_enable;
+  uint32_t lossless_raw_enable;
 } tLHDCV5_DEC_CONFIG;
 
 
